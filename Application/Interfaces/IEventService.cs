5be627e6dfe8754f55248c085a06c5539bdd9497
Denys <lienko.d.k@gmail.com>
2021-11-06 01:56:25 +0200

    add-event-service

--- Application/Interfaces/IEventService.cs
+++ Application/Interfaces/IEventService.cs
@@ -6,9 +6,15 @@ namespace Application.Interfaces
 {
     public interface IEventService
     {
+        Task<IEnumerable<Event>> GetVisitedByMemberAsync(User user);
+        Task<IEnumerable<Event>> GetOrganizedByUserAsync(User user);
+        Task<IEnumerable<Event>> GetBySearchRequestAsync(string searchRequest);
+        Task SubscribeAsync(Event eventModel, User user);
+        Task UnsubscribeAsync(Event eventModel, User user);
+        Task KickMemberAsync(Event eventModel, User user);
+        Task UnkickMemberAsync(Event eventModel, User user);
         Task CreateAsync(Event eventModel);
-        Task<IEnumerable<Event>> GetEventsAsync(int userId);
-        Task Update(Event eventModel);
+        Task UpdateAsync(Event eventModel);
         Task DeleteAsync(Event eventModel);
     }
 }
--- Application/Services/EventService.cs
+++ Application/Services/EventService.cs
@@ -1,9 +1,13 @@
-﻿using Application.Interfaces;
+﻿
+using System;
+using Application.Interfaces;
 using Domain;
+using Domain.Enums;
 using Persistence;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Application.Services
 {
@@ -14,25 +18,78 @@ namespace Application.Services
         {
             _context = context;
         }
+        public async Task<IEnumerable<Event>> GetVisitedByMemberAsync(int userId)
+        {
+            var eventsId = _context.EventMemberships.Select(m => m.EventId).Where(m => m.UserId == userId);
+            return await _context.Events.Where(e => eventsId.Contains(e.Id));
+        }
+        public async Task<IEnumerable<Event>> GetOrganizedByUserAsync(int userId)
+        {
+            var eventsId = _context.EventMemberships.Select(m => m.EventId).Where(m => m.UserId == userId && m.MemberType == MemberType.Organizer);
+            return await _context.Events.Where(e => eventsId.Contains(e.Id));
+        }
+        public async Task<IEnumerable<Event>> GetBySearchRequestAsync(string searchRequest)
+        {
+            return await _context.Events
+                .Where(e => e.Title.Contains(searchRequest))
+                .ToListAsync();
+        }
+        public async Task SubscribeAsync(int eventId, int userId)
+        {
+            var newMembership = new EventMembership(){
+                User = user,
+                Event = eventModel,
+                MemberType = MemberType.Participant
+            };
+            await _context.EventMemberships.AddAsync(newMembership);
+        }
+        public async Task UnsubscribeAsync(int eventId, int userId)
+        {
+            var membershipToDelete = new EventMembership(){
+                User = user,
+                Event = eventModel,
+                MemberType = MemberType.Participant
+            };
+            _context.EventMemberships.Remove(membershipToDelete);
+            await _context.SaveChangesAsync();
+        }
+        public async Task KickMemberAsync(int eventId, int userId)
+        {
+            var membership = await _context.EventMemberships.FindAsync(user.Id, eventModel.Id);
+            try{ 
+                membership.IsKicked = true;
+                _context.Entry(membership).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            catch(ArgumentException ex){
+                
+            }
+        }
+        public async Task UnkickMemberAsync(int eventId, int userId)
+        {
+            var membership = await _context.EventMemberships.FindAsync(user.Id, eventModel.Id);
+            try{ 
+                membership.IsKicked = false;
+                _context.Entry(membership).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            catch(ArgumentException ex){
+                
+            }
+        }
         public async Task CreateAsync(Event eventModel)
         {
             await _context.Events.AddAsync(eventModel);
             await _context.SaveChangesAsync();
         }
 
-        public async Task DeleteAsync(Event eventModel)
+        public async Task DeleteAsync(int eventId)
         {
             _context.Events.Remove(eventModel);
             await _context.SaveChangesAsync();
         }
 
-        public async Task<IEnumerable<Event>> GetEventsAsync(int userId)
-        {
-            var user = await _context.Users.FindAsync(userId);
-            return user.Events;
-        }
-
-        public async Task Update(Event eventModel)
+        public async Task UpdateAsync(Event eventModel)
         {
             _context.Entry(eventModel).State = EntityState.Modified;
             await _context.SaveChangesAsync();
