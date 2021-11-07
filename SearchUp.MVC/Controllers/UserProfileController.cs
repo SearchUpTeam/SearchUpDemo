e481c7bafc7c06d289bb06bc946f7212f9ddaa70
Denys <lienko.d.k@gmail.com>
2021-11-08 00:02:34 +0200

    add-basic-view-for-event-list

--- SearchUp.MVC/Controllers/UserProfileController.cs
+++ SearchUp.MVC/Controllers/UserProfileController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Application.Interfaces;
 
 namespace SearchUp.MVC.Controllers
 {
@@ -11,16 +14,18 @@ namespace SearchUp.MVC.Controllers
     public class UserProfileController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly IEventService _eventService;
 
-        public UserProfileController(UserManager<User> userManager)
+        public UserProfileController(UserManager<User> userManager, IEventService eventService)
         {
             _userManager = userManager;
+            _eventService = eventService;
         }
         [HttpGet]
         public async Task<IActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(User);
-            var profile = new UserProfileViewModel() { Username = user.UserName, About = user.About, Avatars = user.Avatars, Events = user.Events };
+            var profile = new UserProfileViewModel() { Username = user.UserName, About = user.About, Avatars = user.Avatars, Events = _eventService.GetVisitedByUser(user.Id)};
             return View(profile);
         }
     }
