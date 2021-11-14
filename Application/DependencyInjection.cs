815afd5934dc17b1ac591d19217cdb5f1baf91f5
Denys <lienko.d.k@gmail.com>
2021-11-14 20:54:00 +0200

    add-interests-service

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -12,6 +12,7 @@ namespace Application
             services.AddScoped<IEventService, EventService>();
             services.AddScoped<IChatService, ChatService>();
             services.AddScoped<IFollowingService, FollowingService>();
+            services.AddScoped<IInterestsService, InterestsService>();
             return services;
         }
     }
---
+++ Application/Interfaces/IInterestsService.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Domain;
+
+namespace Application.Interfaces
+{
+    public interface IInterestsService
+    {
+        Task<IEnumerable<InterestTag>> GetUserInterestsAsync(int userId);
+        Task AddInterestsToUserAsync(int userId, params int[] interestsId);
+        Task RemoveInterestsFromUserAsync(int userId, params int[] interestsId);
+
+        Task<IEnumerable<InterestTag>> GetEventInterestsAsync(int userId);
+        Task AddInterestsToEventAsync(int userId, params int[] interestsId);
+        Task RemoveInterestsFromEventAsync(int userId, params int[] interestsId);
+
+        Task CreateInterestTagAsync(InterestTag interestTag);
+        Task DeleteInterestTagAsync(int interestId);
+    }
+}
