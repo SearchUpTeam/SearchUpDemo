c054b62861156f25dccd2a55d7e3b47e3d79088d
Denys <lienko.d.k@gmail.com>
2021-12-13 21:56:40 +0200

    add-alternate-key-for-interests

---
+++ Persistence/Migrations/20211211140444_AddAlternateKeyForTags.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddAlternateKeyForTags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "InterestTags",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_InterestTags_Name",
+                table: "InterestTags",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_InterestTags_Name",
+                table: "InterestTags");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "InterestTags",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
