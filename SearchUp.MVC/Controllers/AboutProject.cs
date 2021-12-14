4d74668ba206821e3b186fd850dcf63607240ea8
Viktor <zozulyuk22@gmail.com>
2021-12-14 18:25:37 +0200

    Added the About Us, the About Project, work with file

---
+++ SearchUp.MVC/Controllers/AboutProject.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchUp.MVC.Controllers
+{
+    public class AboutProject : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
